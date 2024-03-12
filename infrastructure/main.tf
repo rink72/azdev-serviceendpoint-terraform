aeda7048be6644096ae56436ffdc295444306b63
Sam <beardmans72@gmail.com>
2024-03-12 21:33:27 +1300

    Add yaml config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,40 +1,20 @@
 locals {
-    project_name = "lab"
+  endpoint_definitions = yamldecode(file("${path.root}/../configuration/endpoints.yml"))["endpoints"]
+  projects             = yamldecode(file("${path.root}/../configuration/group-one.yml"))["projects"]
 
+  endpoint_map = {
+    for type, endpoints in local.endpoint_definitions :
+    type => { for endpoint in endpoints : endpoint.endpoint_name => endpoint }
+  }
 }
 
 module "azdev_projects" {
-    source = "../modules/azdev-serviceendpoint"
-    project_name = local.project_name
+  for_each = { for idx, project in local.projects : idx => project }
+  source   = "../modules/azdev-serviceendpoint"
 
-    azurerm_endpoints = {
-        "dev" = {
-            endpoint_name = "dev"
-            description = "dev"
-            subscription_id = "b530b5ac-8ca6-4393-b35f-068dcb970647"
-            subscription_name = "dev"
-            service_principal_client_id = "f1e435f2-387d-4b91-949f-8e5eb8ab038b"
-            tenant_id = "54406fc1-bb8f-46c7-ae4d-4e4e563f1d5d"
-        }
-    }
+  project_name = each.value.name
 
-    sonarcloud_endpoints = {
-        "sonarcloud" = {
-            endpoint_name = "sonarcloud-sve"
-            description = "sonarcloud-sve"
-            keyvault_name = "rink72-shared-kv"
-            keyvault_secret_name = "sonarcloud-key"
-            keyvault_resource_group = "shared-infra"
-        }
-    }
-
-    snyk_endpoints = {
-        "snyk" = {
-            endpoint_name = "snyk-sve"
-            description = "snyk-sve"
-            keyvault_name = "rink72-shared-kv"
-            keyvault_secret_name = "snyk-secret"
-            keyvault_resource_group = "shared-infra"
-        }
-    }
+  sonarcloud_endpoints = { for name in lookup(each.value, "sonarcloud_endpoints", []) : name => local.endpoint_map["sonarcloud"][name] }
+  azurerm_endpoints    = { for name in lookup(each.value, "azurerm_endpoints", []) : name => local.endpoint_map["azurerm"][name] }
+  snyk_endpoints       = { for name in lookup(each.value, "snyk_endpoints", []) : name => local.endpoint_map["snyk"][name] }
 }
